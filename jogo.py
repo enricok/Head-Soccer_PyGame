50c549ae666c085bb1857b84e6e50806e0ec6a4f
enricok <enricogkarsten@gmail.com>
2024-10-29 12:44:09 -0300

    Tela de ínicio com texto

--- jogo.py
+++ jogo.py
@@ -5,11 +5,20 @@ import pygame
 pygame.init()
 
 # ----- Gera tela principal
-window = pygame.display.set_mode((500, 400))
-pygame.display.set_caption('Hello World!')
+WIDTH = 700
+HEIGHT = 400
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Head Soccer!')
+logo_do_jogo = pygame.image.load('assets/img/5t.png').convert()
+logo_do_jogo = pygame.transform.scale(logo_do_jogo, (700, 400))
+font = pygame.font.SysFont(None, 25)
+start = font.render('press "SPACE" to play', True, (255, 255, 255))
 
 # ----- Inicia estruturas de dados
 game = True
+screen = 1
+piscar_texto = 0
+mostrar = True
 
 # ===== Loop principal =====
 while game:
@@ -18,12 +27,36 @@ while game:
         # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
+            screen = 1
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                screen = 2
 
-    # ----- Gera saídas
-    window.fill((255, 255, 255))  # Preenche com a cor branca
+    #código tirado do https://stackoverflow.com/questions/42472019/flickering-text-in-pygame
+
+    if piscar_texto >= 600: 
+        mostrar = not mostrar  
+        piscar_texto = 0  
+
+    ##
+
+    if screen == 1:
+        window.fill((255,245,255))  # Preenche com a cor branca
+        window.blit(logo_do_jogo, (0, 0))
+        if mostrar == True:
+            window.blit (start, (256, 231))
+    else:
+        window.fill((188,143,143))
+        
+
+
+    
+    piscar_texto += 1
+
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
-pygame.quit()
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
